0c156f31e505b0709039aeaac21c9c03e80c2c2c
Harris Stevenson <harris6144@gmail.com>
2021-03-14 13:31:49 +0000

    Create core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
+import {CoreModule} from './core/core.module';
 
 import {AppComponent} from './app.component';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule],
+    imports: [BrowserModule, AppRoutingModule, CoreModule],
     providers: [],
     bootstrap: [AppComponent]
 })
