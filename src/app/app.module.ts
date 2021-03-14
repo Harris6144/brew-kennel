65c94b935a516e8ab950afaea5ad55874fec273a
Harris Stevenson <harris6144@gmail.com>
2021-03-14 14:22:25 +0000

    Create brews module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,13 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {CoreModule} from './core/core.module';
+import {BrewsModule} from './brews/brews.module';
 
 import {AppComponent} from './app.component';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule, CoreModule],
+    imports: [BrowserModule, AppRoutingModule, CoreModule, BrewsModule],
     providers: [],
     bootstrap: [AppComponent]
 })
