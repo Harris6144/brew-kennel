bccda60cf9e84546d9fd7af7b1c84a3ecb4ad3b7
Harris Stevenson <harris6144@gmail.com>
2021-03-14 14:28:45 +0000

    Create brews routing module

--- src/app/brews/brews.module.ts
+++ src/app/brews/brews.module.ts
@@ -1,10 +1,11 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {BrewsRoutingModule} from './brews-routing.module';
 
 @NgModule({
     declarations: [],
-    imports: [CommonModule]
+    imports: [CommonModule, BrewsRoutingModule]
 })
 export class BrewsModule {
-    
+
 }
